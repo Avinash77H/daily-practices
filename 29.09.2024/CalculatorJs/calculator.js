6dcdf87a1befae8b5fef539ac9a53d8e69bad3cb
Avinash77H <arakholiya5@gmail.com>
2024-09-30 15:57:50 +0530

    calculator project update

--- 29.09.2024/CalculatorJs/calculator.js
+++ 29.09.2024/CalculatorJs/calculator.js
@@ -1,2 +1,34 @@
+
+
 let input = document.querySelector(".display")
-console.log(input)
+let output = []
+function add(args){
+ if(args === 'AC'){
+  input.value = '';
+  output = []
+ }  
+ else if(args === "DEL"){
+  output.pop()
+  input.value = output.join("")
+
+ }
+ else{
+  output.push(args)
+  input.value = output.join("")
+
+ }
+  console.log(output)
+
+}
+
+ function total(){
+  try{
+    input.value = eval(output.join(""))
+    output = []
+    output.push(input.value)
+  }
+  catch(err){
+    console.log("ERR")
+  }
+  
+ }
