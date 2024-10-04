dac6ab3edc198ea18c3f5158ad65d70bc1da5429
Avinash77H <arakholiya5@gmail.com>
2024-10-04 14:35:15 +0530

    04.10.2024

--- 03.10.2024/BasicTask/src/Components/Timer.jsx
+++ 03.10.2024/BasicTask/src/Components/Timer.jsx
@@ -1,32 +1,46 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Timer(){
-    const[count,setCount]= useState(0)
-    
-    
+    const[second,setSecond]= useState(0);
+    const[minute,setMinute]= useState(0);
 
-    const fun= setInterval(()=>{
-          setCount(count => count + 1)
-      },1000)
+    let timer;
+   useEffect(()=>{
+    timer = setInterval(()=>{
+      setSecond(second + 1);
+      if(second === 59){
+        setMinute(minute + 1);
+        setSecond(0)
+      }
+    },1000)
+  
+    return ()=>clearInterval(timer) 
+   })
+   
 
     function Start(){
         
     }
 
     function Stop(){
-        clearInterval(fun)
+        clearInterval(timer)
+    }
+
+    function Restart(){
+      setSecond(0)
+      setMinute(0)
     }
 
   return (
     <div className='w-screen h-screen flex justify-center items-center '>
       <div className='border-2 border-black bg-green-100 p-8 flex flex-col items-center gap-4'>
         <div>
-            <p>{count}</p>
+            <p>{minute <10?"0"+minute:minute}:{second < 10 ? "0"+second:second}</p>
         </div>
         <div className='flex gap-4'>
-            <button onClick={Start} className='bg-green-500 px-2 py-1 rounded-xl text-white'>Start</button>
+            <button  className='bg-green-500 px-2 py-1 rounded-xl text-white'>Start</button>
             <button onClick={Stop} className='bg-red-500 px-2 py-1 rounded-xl text-white'>Stop</button>
-            <button className='bg-violet-500 px-2 py-1 rounded-xl text-white'>Reset</button>
+            <button onClick={Restart} className='bg-violet-500 px-2 py-1 rounded-xl text-white'>Reset</button>
         </div>
       </div>
     </div>
