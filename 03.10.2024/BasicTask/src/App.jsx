9dab99b324d8d1d9bd78a10a27856b5be87eb834
avinash <arakholiya5@gmail.com>
2024-10-05 11:25:19 +0530

    05.10.2024

--- 03.10.2024/BasicTask/src/App.jsx
+++ 03.10.2024/BasicTask/src/App.jsx
@@ -1,46 +1,48 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
-import IfElse from './Components/IfElse'
-import Ternary from './Components/Ternary'
-import Logical from './Components/Logical'
-import Switch from './Components/Switch'
-import Nesting from './Components/Nesting'
-import Form from './Components/Form'
-import ListRendering from './Components/ListRendering'
-import Card from './Components/Card'
-import ObjectState from './Components/ObjectState'
-import ArrayState from './Components/ArrayState'
-import Counter from './Components/Counter'
-import FullForm from './Components/FullForm'
-import Timer from './Components/Timer'
+import "./App.css";
+import IfElse from "./Components/IfElse";
+import Ternary from "./Components/Ternary";
+import Logical from "./Components/Logical";
+import Switch from "./Components/Switch";
+import Nesting from "./Components/Nesting";
+import Form from "./Components/Form";
+import ListRendering from "./Components/ListRendering";
+import Card from "./Components/Card";
+import ObjectState from "./Components/ObjectState";
+import ArrayState from "./Components/ArrayState";
+import Counter from "./Components/Counter";
+import FullForm from "./Components/FullForm";
+import Timer from "./Components/Timer";
 
 
 function App() {
   const data = [
     {
-      name:'avinash',
-      age:23
+      name: "avinash",
+      age: 23,
     },
     {
-      name:'milan',
-      age:22
+      name: "milan",
+      age: 22,
     },
     {
-      name:'ugam',
-      age:24
+      name: "ugam",
+      age: 24,
     },
     {
-      name:'shyam',
-      age:15
+      name: "shyam",
+      age: 15,
     },
     {
-      name:'ram',
-      age:18
-    }
-  ]
- const car = ['volvo','bugati','audi']
-//  const cars = []
+      name: "ram",
+      age: 18,
+    },
+  ];
+  const car = ["volvo", "bugati", "audi"];
+
+  const [timer, setTimer] = useState(true);
+  //  const cars = []
   return (
     <>
       {/* <IfElse answer={true}/> */}
@@ -55,8 +57,12 @@ function App() {
       {/* <Counter/> */}
       {/* <FullForm/> */}
       <Timer/>
+
+   
+
+     
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- 03.10.2024/BasicTask/src/Components/Timer.jsx
+++ 03.10.2024/BasicTask/src/Components/Timer.jsx
@@ -1,50 +1,65 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
-function Timer(){
-    const[second,setSecond]= useState(0);
-    const[minute,setMinute]= useState(0);
+function Timer() {
+  const [second, setSecond] = useState(0);
+  const [minute, setMinute] = useState(0);
 
-    let timer;
-   useEffect(()=>{
-    timer = setInterval(()=>{
+  let timer;
+  useEffect(() => {
+    timer = setInterval(() => {
+    
       setSecond(second + 1);
-      if(second === 59){
-        setMinute(minute + 1);
-        setSecond(0)
+      console.log('interval run')
+      if (second === 59) {
+        setMinute(minute => minute + 1);
+        setSecond(0);
       }
-    },1000)
-  
-    return ()=>clearInterval(timer) 
-   })
-   
+    }, 1000);
 
-    function Start(){
-        
-    }
+    return () => {
+      clearInterval(timer);
+      console.log("clear");
+    };
+  });
 
-    function Stop(){
-        clearInterval(timer)
-    }
+  function Stop() {
+    clearInterval(timer);
+  }
 
-    function Restart(){
-      setSecond(0)
-      setMinute(0)
-    }
+  function Restart() {
+    setSecond(0);
+    setMinute(0);
+  }
 
   return (
-    <div className='w-screen h-screen flex justify-center items-center '>
-      <div className='border-2 border-black bg-green-100 p-8 flex flex-col items-center gap-4'>
+    <div className="w-screen h-screen flex justify-center items-center ">
+      <div className="border-2 border-black bg-green-100 p-8 flex flex-col items-center gap-4">
         <div>
-            <p>{minute <10?"0"+minute:minute}:{second < 10 ? "0"+second:second}</p>
+          <p>
+            {minute < 10 ? "0" + minute : minute}:
+            {second < 10 ? "0" + second : second}
+          </p>
         </div>
-        <div className='flex gap-4'>
-            <button  className='bg-green-500 px-2 py-1 rounded-xl text-white'>Start</button>
-            <button onClick={Stop} className='bg-red-500 px-2 py-1 rounded-xl text-white'>Stop</button>
-            <button onClick={Restart} className='bg-violet-500 px-2 py-1 rounded-xl text-white'>Reset</button>
+        <div className="flex gap-4">
+          <button className="bg-green-500 px-2 py-1 rounded-xl text-white">
+            Start
+          </button>
+          <button
+            onClick={Stop}
+            className="bg-red-500 px-2 py-1 rounded-xl text-white"
+          >
+            Stop
+          </button>
+          <button
+            onClick={Restart}
+            className="bg-violet-500 px-2 py-1 rounded-xl text-white"
+          >
+            Reset
+          </button>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Timer
+export default Timer;
