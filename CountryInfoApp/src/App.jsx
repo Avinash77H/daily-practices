3c351131273681cd8a88db2f42dbddafe728812c
avinash <arakholiya5@gmail.com>
2024-10-19 09:05:22 +0530

    19.10.2024

---
+++ CountryInfoApp/src/App.jsx
@@ -0,0 +1,58 @@
+
+import "./App.css";
+
+const countryData = [
+  {
+    name:'India',
+    capital:'Delhi',
+    img:'https://static.toiimg.com/photo/msid-24245804,width-96,height-65.cms',
+    info:"Sovereign Socialist Secular Democratic Republic with a Parliamentary system of Government."
+  },
+  {
+    name:'United States',
+    capital:' Washington, D.C.',
+    img:'https://images.pexels.com/photos/356844/pexels-photo-356844.jpeg?cs=srgb&dl=pexels-pixabay-356844.jpg&fm=jpg',
+    info:" Constitution-based federal republic."
+  },
+  {
+    name:'South Africa',
+    capital:'Bloemfontein',
+    img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6vRgif_X4G5pzbGSThu2ZB1CVZ7IiDHF-ObsWHAHUJ29MDdMZUuAZIYIPiJUC-knJTJY&usqp=CAU',
+    info:"South Africa, the southernmost country on the African continent, renowned for its varied topography, great natural beauty, and cultural diversity, all of which have made the country a favored destination for travelers since the legal ending of apartheid (Afrikaans: “apartness,” or racial separation) in 1994."
+  },
+  {
+    name:'Canada',
+    capital:'Ottawa',
+    img:'https://adventures.com/media/19780/s-sunrise-sunset-ottawa-city-canada-buildings-architecture.jpg',
+    info:"Ottawa, city, capital of Canada, located in southeastern Ontario. In the eastern extreme of the province, Ottawa is situated on the south bank of the Ottawa River across from Gatineau, Quebec, at the confluence of the Ottawa (Outaouais), Gatineau, and Rideau rivers."
+  },
+  {
+    name:'India',
+    capital:'Delhi',
+    img:'https://static.toiimg.com/photo/msid-24245804,width-96,height-65.cms',
+    info:"Sovereign Socialist Secular Democratic Republic with a Parliamentary system of Government."
+  },
+  {
+    name:'India',
+    capital:'Delhi',
+    img:'https://static.toiimg.com/photo/msid-24245804,width-96,height-65.cms',
+    info:"Sovereign Socialist Secular Democratic Republic with a Parliamentary system of Government."
+  },
+  {
+    name:'India',
+    capital:'Delhi',
+    img:'https://static.toiimg.com/photo/msid-24245804,width-96,height-65.cms',
+    info:"Sovereign Socialist Secular Democratic Republic with a Parliamentary system of Government."
+  }
+]
+
+function App() {
+ 
+  return (
+    <div>
+    
+    </div>
+  );
+}
+
+export default App;
